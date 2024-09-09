749be89b403958aea57246547fb5012f44617ba7
Xiomara Cárdenas <xiomarach2000@gmail.com>
2024-09-08 23:32:54 -0500

    Secondary (#5)
    * Se agrega barra de carga y se agregan mas etiquetas img
    
    * Se redirecciona ruta de acceso para etiqueta img y se quita etiqueta img que contenia logo
    
    * Se saca Images/ de la carpeta src/
    
    * Se agregan nuevas imagenes y se elimina una de ellas
    
    * Se agrega principio de funcionalidad para barra de carga
    
    * Se agregan nuevos estilos
    
    * Se saca Index.js de carpeta src/
    
    * Se saca style.css de carpeta src/
    
    * Se redirecciona ruta de acceso para etiqueta img
    
    * Se agregan nuevas imagenes, se eliminan otras; se agregan nuevos estilos; se modifican los index quitando lo que nos es necesario y se replantean algunas etiquetas; se intenta hacer funcionalidad de la barra de carga a través de javascript pero aún no está lista

---
+++ index.js
@@ -0,0 +1,6 @@
+ /*const progress_bar = document.getElementById("welcome");
+setTimeout(function () {
+    progress_bar.classList.add("close");
+    document.getElementById("homeTwo").style.display = "flex";
+    visivilityCard(pokemons);
+}, 9000);*/
