29360705edc517e5657baea476f31a08174cff5d
Xiomara Cárdenas <xiomarach2000@gmail.com>
2024-09-29 21:22:25 -0500

    Se termina funcionalidad de barra de carga de página inicial, queda en correcto funcionamiento

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
- /*const progress_bar = document.getElementById("welcome");
+const progress_bar = document.getElementById("welcome");
 setTimeout(function () {
     progress_bar.classList.add("close");
-    document.getElementById("homeTwo").style.display = "flex";
-    visivilityCard(pokemons);
-}, 9000);*/
+    console.log ("progress_bar")
+    window.location.href = "index2.html"
+}, 9000);
